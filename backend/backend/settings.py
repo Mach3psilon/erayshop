5980e0f4bd561691f1365edc97b3c7c0a4838129
Machepsilon <erayokutay@hotmail.com>
2022-11-20 19:29:44 +0300

    Add create, update product endpoint and functions

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -179,7 +179,6 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -5,7 +5,8 @@ import { useNavigate, useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
-import { listProducts, deleteProduct} from '../actions/productActions'
+import { listProducts, deleteProduct, createProduct} from '../actions/productActions'
+import {PRODUCT_CREATE_RESET} from '../constants/productConstants'
 
 function ProductListScreen() {
 
@@ -19,20 +20,29 @@ function ProductListScreen() {
     const productDelete = useSelector(state => state.productDelete)
     const { loading: loadingDelete, error: errorDelete, success: successDelete } = productDelete
 
+    const productCreate = useSelector(state => state.productCreate)
+    const { loading: loadingCreate, error: errorCreate, success: successCreate, product: createdProduct } = productCreate
+
+
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
+
 
    
 
     useEffect(() => {
-        if(userInfo && userInfo.isAdmin) {
-            dispatch(listProducts())
+        dispatch({type: PRODUCT_CREATE_RESET})
+        if(!userInfo.isAdmin) {
+            history('/login')
+            
 
         }
-        else {
-            history('/login')
+        if(successCreate) {
+            history(`/admin/product/${createdProduct._id}/edit`)
+        } else {
+            dispatch(listProducts())
         }
-    }, [dispatch, history, userInfo, successDelete])
+    }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct])
 
     const deleteHandler = (id) => {
         if (window.confirm('Are you sure you want to delete this product?')) {
@@ -43,6 +53,7 @@ function ProductListScreen() {
     }
 
     const createProductHandler = (product) => {
+        dispatch(createProduct())
         
     }
 
@@ -63,6 +74,8 @@ function ProductListScreen() {
         {loadingDelete && <Loader />}
         {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
         
+        {loadingCreate && <Loader />}
+        {errorCreate && <Message variant='danger'>{errorCreate}</Message>}
            
       {(loading) ? (<Loader />) : (error) ? (<Message variant='danger'>{error}</Message>) : (
     
