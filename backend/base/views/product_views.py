6893674d945c3ce2f04f5ad13f6703e1cd8a6a7c
Machepsilon <erayokutay@hotmail.com>
2022-11-26 17:29:57 +0300

    Add review feature

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 from django.contrib.auth.models import User
-from base.models import Product
+from base.models import Product, Review
 from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 
@@ -75,3 +75,50 @@ def uploadImage(request):
     product.save()
 
     return Response('Image was uploaded')
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def createProductReview(request, pk):
+    user = request.user
+    product = Product.objects.get(_id=pk)
+    data = request.data
+
+    # 1) Review already exists
+    alreadyExists = product.review_set.filter(user=user).exists()
+    
+    if alreadyExists:
+        content = {'detail': 'Product already reviewed'}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        # 2) No rating or review
+        try: 
+
+            if data['rating'] == 0 or data['comment'] == '':
+                content = {'detail': 'Please add rating and comment'}
+                return Response(content, status=status.HTTP_400_BAD_REQUEST)
+            # 3) Create review
+            else:
+                review = Review.objects.create(
+                    user=user,
+                    product=product,
+                    name=user.first_name,
+                    rating=data['rating'],
+                    comment=data['comment']
+                )
+
+                reviews = product.review_set.all()
+                product.numReviews = len(reviews)
+
+                total = 0
+                for i in reviews:
+                    total += i.rating
+
+                product.rating = total / len(reviews)
+                product.save()
+        except:
+            content = {'detail': 'Please add rating and comment'}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+    return Response('Review added')
+ 
+   
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -5,29 +5,54 @@ import { Row, Col, Image, ListGroup, Button, Card, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
-import { listProductDetails } from '../actions/productActions'
-
+import { listProductDetails, createProductReview } from '../actions/productActions'
+import { PRODUCT_CREATE_REVIEW_RESET } from '../constants/productConstants'
 
 
 
 function ProductScreen({ match}) {
     const [qty, setQty] = useState(1)
+    const [rating, setRating] = useState(0)
+    const [comment, setComment] = useState('')
     
     const dispatch = useDispatch()
+
     const productDetails = useSelector(state => state.productDetails)
     const { loading, error, product } = productDetails
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
+
+    const productCreateReview = useSelector(state => state.productCreateReview)
+    const { success: successProductReview, error: errorProductReview, loading: loadingProductReview } = productCreateReview
 
    const { id } = useParams();
     
     useEffect(() => {
+        if(successProductReview) {
+            alert('Review Submitted!')
+            setRating(0)
+            setComment('')
+            dispatch({ type: PRODUCT_CREATE_REVIEW_RESET })
+        }
         dispatch(listProductDetails(id))
         
-    }, [dispatch, id])
+    }, [dispatch, id, successProductReview])
+
     const history = useNavigate()
     const addToCartHandler = () => {
         
         history(`/cart/${id}?qty=${qty}`)
         
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        dispatch(createProductReview(id, {
+            rating,
+            comment
+        }))
+        setComment('')
     }
 
 
@@ -37,6 +62,7 @@ function ProductScreen({ match}) {
      <Link to="/" className='btn btn-light my-3'>Go Back</Link>
 
         {loading ? <Loader/> : error ? <Message variant='danger'>{error}</Message> : 
+        <div>
         <Row>
             <Col md={6}>
                 <Image src={product.image} alt={product.name} fluid/>
@@ -116,6 +142,55 @@ function ProductScreen({ match}) {
                 </Card>
                 </Col>
         </Row>
+        <Row>
+            <Col md={6}>
+                <h2>Reviews</h2>
+                {product.reviews.length === 0 && <Message>No Reviews</Message>}
+                <ListGroup variant='flush'>
+                    {product.reviews.map(review => (
+                        <ListGroup.Item key={review._id}>
+                            <strong>{review.name}</strong>
+                            <Rating value={review.rating} color={"#f8e825"}/>
+                            <p>{review.createdAt.substring(0, 10)}</p>
+                            <p>{review.comment}</p>
+                        </ListGroup.Item>
+                    ))}
+                    <ListGroup.Item>
+                        <h2>Write a Customer Review</h2>
+                        {successProductReview && <Message variant='success'>Review submitted successfully</Message>}
+                        {loadingProductReview && <Loader/>}
+                        {errorProductReview && <Message variant='danger'>{errorProductReview}</Message>}
+                        {userInfo ? (
+                            <Form onSubmit={submitHandler}>
+
+                                <Form.Group controlId='rating'> 
+                                    <Form.Label>Rating</Form.Label>
+                                    <Form.Control as='select' value={rating} onChange={(e) => setRating(e.target.value)}>
+                                        <option value=''>Select...</option>
+                                        <option value='1'>1 - Poor</option>
+                                        <option value='2'>2 - Fair</option>
+                                        <option value='3'>3 - Good</option>
+                                        <option value='4'>4 - Very Good</option>
+                                        <option value='5'>5 - Excellent</option>
+                                    </Form.Control>
+                                </Form.Group>
+
+                                <Form.Group controlId='comment'>
+                                    <Form.Label>Comment</Form.Label>
+                                    <Form.Control as='textarea' row='3' value={comment} onChange={(e) => setComment(e.target.value)}></Form.Control>
+                                </Form.Group>
+                        
+                                <Button disabled={loadingProductReview} type='submit' variant='primary'>
+                                    Submit
+                                </Button>
+                            </Form>
+                        ) : <Message>Please <Link to='/login'>sign in</Link> to write a review</Message>}
+                    </ListGroup.Item>
+                </ListGroup>
+            </Col>
+        </Row>
+
+        </div>
         }
         
             
